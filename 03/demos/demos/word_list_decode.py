02743ea98b3e25970ae3f9e9fbaf815fdb0d47c9
Ned Bellavance <nbellavance@anexinet.com>
2018-06-01 16:00:03 -0400

    finished module

--- 03/demos/demos/word_list_decode.py
+++ 03/demos/demos/word_list_decode.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python3
+"""Retrieve and print words from a URL.
+
+Usage:
+    python word_list_decode.py <URL>
+"""
+
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+    
+    Args:
+        url: the URL of a UTF-8 text document.
+    
+    Returns:
+        A list of strings containing words from the document
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -13,11 +28,21 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 
 def main(url):
+    """Print each work from a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
